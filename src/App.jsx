0976101f8ab64297608ec016f288e128c7a9de1b
Raquel <quelcaraquel1@gmail.com>
2025-02-11 11:00:54 -0400

    Navbar an Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import styles from './App.module.css'
+import Hero from './components/Hero/Hero'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
-    <>
-      Hello World
-    </>
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+    </div>
   )
 }
 
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,26 @@
+import styles from "./Hero.module.css";
+import heroImage from "./../../../assets/hero/heroImage.png";
+
+const Hero = () => {
+    return (
+        <section className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Hola, soy Raquel</h1>
+                <p className={styles.description}>
+                    Soy desarrolladora de software con interés en el desarrollo web y móvil. Me gusta resolver problemas y aprender nuevas tecnologías. ¡Hablemos!                </p>
+                <a href="mailto:quelcaraquel1@gmail.com" className={styles.contactBtn}>
+                    Contactame
+                </a>
+            </div>
+            <img
+                src={heroImage}
+                alt="Hero image of me"
+                className={styles.heroImg}
+            />
+            <div className={styles.topBlur} />
+            <div className={styles.bottomBlur} />
+        </section>
+    )
+}
+
+export default Hero
